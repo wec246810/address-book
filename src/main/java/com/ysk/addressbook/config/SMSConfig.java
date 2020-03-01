e71d06f92f9b657a5ed41bb02fb2a96b1ba37bf9
wec246810 <17754321391@163.com>
2020-03-01 22:35:06 +0800

    Update SMSConfig.java

--- src/main/java/com/ysk/addressbook/config/SMSConfig.java
+++ src/main/java/com/ysk/addressbook/config/SMSConfig.java
@@ -7,11 +7,11 @@ package com.ysk.addressbook.config;
 public class SMSConfig {
     // 短信应用SDK AppID
 //   public final int appid = 1400009099; // 1400开头
-    public static final int appid = 1400086229;
+    public static final int appid = xxx;
     // 短信应用SDK AppKey
 //    public final String appkey = "9ff91d87c2cd7cd0ea762f141975d1df37481d48700d70ac37470aefc60f9bad";
 
-    public static final String appkey = "0984206d87fcbc47847d1ce38ce8c302";
+    public static final String appkey = "xxx";
 
     // 短信模板ID，需要在短信应用中申请
     public static final int templateId = 1; // NOTE: 这里的模板ID`7839`只是一个示例，真实的模板ID需要在短信控制台中申请
